0c989c9733239cd79b3de62fa68734adbeb2fcc8
BohaHU <102597307+BohaHU@users.noreply.github.com>
2022-04-19 23:54:05 +0800

    Add personal project Yekun Liu.

---
+++ Assets/Yekun Liu/script/CameraControlOne.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlOne : MonoBehaviour
+{
+    [Range(0, 1)]
+    [SerializeField]
+    float target_offsety = 0.6f;   
+
+    [Range(0, 90)]
+    [SerializeField]
+    float initRotate = 15f;     
+
+    [Range(2, 10)]
+    [SerializeField]
+    float DISTANCE_DEAFULT = 3.2f;  
+
+    public Transform target;  
+
+    private float distance;
+
+
+    private Vector3 playerTarget;
+
+    private float lastRotate;
+
+    // Use this for initialization
+    void Start()
+    {
+        distance = DISTANCE_DEAFULT;    
+        //target = GameObject.FindGameObjectWithTag("Player").transform;   
+        playerTarget = new Vector3(target.position.x, target.position.y + target_offsety, target.position.z);   
+        Quaternion cr = Quaternion.Euler(initRotate, transform.eulerAngles.y, 0);   
+        //计算摄像机的位置
+        Vector3 positon = playerTarget;
+        positon += cr * Vector3.back * distance;
+        transform.position = positon;
+        transform.rotation = target.rotation;
+        transform.LookAt(playerTarget);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(1))
+        {                                   
+            transform.RotateAround(playerTarget, Vector3.up, Input.GetAxis("Mouse X") * 3f);
+        }
+    }
+    
+    void LateUpdate()
+    {
+        playerTarget = new Vector3(target.position.x, target.position.y + target_offsety, target.position.z);
+        Quaternion cr = Quaternion.Euler(initRotate, transform.eulerAngles.y, 0);
+        Vector3 positon = playerTarget + (cr * Vector3.back * distance);
+        RaycastHit[] hits = Physics.RaycastAll(new Ray(playerTarget, (positon - playerTarget).normalized));
+        distance = DISTANCE_DEAFULT;
+        if (hits.Length > 0)
+        {
+            RaycastHit stand = new RaycastHit();
+            float maxDistance = float.MaxValue;
+            foreach (RaycastHit hit in hits)
+            {
+                if (!hit.collider.isTrigger && hit.collider.name != target.name && hit.distance < maxDistance)
+                {
+                    stand = hit;
+                    maxDistance = stand.distance;
+                }
+            }
+            if (stand.collider != null)
+            {
+                string tag = stand.collider.gameObject.tag;
+                
+                distance = Vector3.Distance(stand.point, playerTarget);
+                if (distance > DISTANCE_DEAFULT)
+                {
+                    distance = DISTANCE_DEAFULT;
+                }
+            }
+        }
+        positon = playerTarget + (cr * Vector3.back * distance);
+        transform.position = Vector3.Lerp(transform.position, positon, 0.5f);  
+        
+        Debug.DrawRay(playerTarget, positon - playerTarget, Color.red);
+        if (lastRotate != initRotate)
+        {
+            transform.LookAt(playerTarget);
+            lastRotate = initRotate;
+        }
+
+        
+        float fov = this.GetComponent<Camera>().fieldOfView;
+        fov -= Input.GetAxis("Mouse ScrollWheel") * 10;
+       
+        this.GetComponent<Camera>().fieldOfView = Mathf.Clamp(fov, 30, 50);
+    }
+}
---
+++ Assets/Yekun Liu/script/objMove.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objMove : MonoBehaviour
+{
+    Transform myCamera;  
+    private CharacterController cc;
+    Vector3 playerDirect;        
+    Vector3 correctDirect;       
+    float perError = 0.2f;       
+    float speed = 0.1f;            
+    Vector3 front;  
+    Vector3 back;  
+    Vector3 left;    
+    Vector3 right; 
+    public Animator ani;
+    private bool ismove;
+
+    public GameObject keytext1;
+    public GameObject keyytext2;
+    public Text timetext;
+    private float time = 120;
+    private int times;
+    public GameObject YouWin;
+    public GameObject music;
+
+    public GameObject GameOver;
+    
+    private float number;
+   
+    void Start()
+    {
+        myCamera = Camera.main.transform;  
+        front = Vector3.zero;
+        back = Vector3.zero;
+        left = Vector3.zero;
+        right = Vector3.zero;
+        correctDirect = Vector3.zero;
+        cc = transform.GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        if (time>=0)
+        {
+            time -= Time.deltaTime;
+            times = (int)time % 181;  
+            timetext.text = string.Format("Time :{0}", times);
+        }
+       
+        if (time<=0)
+        {
+            GameOver.SetActive(true);
+        
+            speed = 0;
+        }
+        if (number == 2 || number >= 0)
+        {
+            number -= Time.deltaTime;
+            if (number <= 0)
+            {
+               
+                keytext1.SetActive(false);
+                number = 0;
+            }
+        }
+        if (number == 3 || number >= 0)
+        {
+            number -= Time.deltaTime;
+            if (number <= 0)
+            {
+                keyytext2.SetActive(false);
+                number = 0;
+            }
+        }
+        PlayerControl();
+    }
+    private void CalculateDirection()
+    {
+        Vector3 temp = transform.position - myCamera.position;
+        temp = new Vector3(temp.x, 0, temp.z);
+        front = temp;  
+
+
+        if (correctDirect != Vector3.zero && Mathf.Abs(correctDirect.x - front.x) < perError && Mathf.Abs(correctDirect.z - front.z) < perError)  
+        {
+            front = correctDirect;
+        }
+        back = -front;
+        left = new Vector3(-front.z, 0, front.x);
+        right = -left;
+        correctDirect = front;
+    }
+
+    /// <summary>
+    /// 控制角色移动
+    /// </summary>
+    private void PlayerControl()
+    {
+        playerDirect = new Vector3(0, 0, 0);
+
+        CalculateDirection();    
+
+        if (Input.GetAxis("Horizontal") < 0)  
+        {
+            playerDirect = left;
+        }
+        if (Input.GetAxis("Horizontal") > 0)  
+        {
+            playerDirect = right;
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            if (Input.GetAxis("Horizontal") < 0)  
+            {
+                playerDirect = front + left;
+            }
+            else if (Input.GetAxis("Horizontal") > 0) 
+            {
+                playerDirect = front + right;
+            }
+            else
+            {
+                playerDirect = front;     
+            }
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            if (Input.GetAxis("Horizontal") < 0)   
+            {
+                playerDirect = back + left;
+            }
+            else if (Input.GetAxis("Horizontal") > 0)   
+            {
+                playerDirect = back + right;
+            }
+            else
+            {
+                playerDirect = back;     
+            }
+        }
+        if (playerDirect != Vector3.zero)
+        {
+            playerDirect += transform.position;
+            MoveSpeed(playerDirect);
+        }
+        else
+        {
+            if (ismove)
+            {
+                ismove = false;
+                ani.SetBool("idle_bool", true);
+                ani.SetBool("run_bool", false);
+            }
+
+
+        }
+    }
+
+
+    private void MoveSpeed(Vector3 direct)
+    {
+        ismove = true;
+        ani.SetBool("idle_bool", false);
+        ani.SetBool("run_bool", true);
+        transform.LookAt(new Vector3(direct.x, transform.position.y, direct.z));
+        cc.Move(transform.forward * speed);
+    }
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "key")
+        {
+            keytext1.SetActive(true);
+            Destroy(other.gameObject);
+            number = 2;
+        }
+        if (other.gameObject.tag == "keyy")
+        {
+            keyytext2.SetActive(true);
+            Destroy(other.gameObject);
+            number = 3;
+            music.gameObject.name = "Realkey";
+        }
+        if (other.gameObject.tag == "keyexit")
+        {
+            if (music.gameObject.name == "Realkey")
+            {
+                YouWin.SetActive(true);
+               
+                speed = 0;
+            }
+        }
+    }
+}
